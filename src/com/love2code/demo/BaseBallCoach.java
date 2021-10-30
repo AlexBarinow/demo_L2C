1b6f7320dfeea66336b845956ca4c4bb26eec45d
Alex Johnson <alexjohnson@Alexs-MacBook-Pro.local>
2021-10-31 00:32:16 +0300

    first commit

---
+++ src/com/love2code/demo/BaseBallCoach.java
@@ -0,0 +1,8 @@
+package com.love2code.demo;
+
+public class BaseBallCoach {
+
+    public String getDailyWorkout(){
+        return "spend 30 mins on batting procedure";
+    }
+}
---
+++ src/com/love2code/demo/myApp.java
@@ -0,0 +1,14 @@
+package com.love2code.demo;
+
+public class myApp {
+
+    public static void main(String[] args) {
+
+        //create the object
+
+        BaseBallCoach baseBallCoach = new BaseBallCoach();
+
+        System.out.println(baseBallCoach.getDailyWorkout());
+
+    }
+}
